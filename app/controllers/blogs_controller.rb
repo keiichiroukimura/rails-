0b7113c453fcea1c94a09c6184621e2a6224798e
keiichiroukimura <kkhtwdect@gmail.com>
2019-03-14 09:09:53 +0000

    init

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -41,12 +41,12 @@ class BlogsController < ApplicationController
       redirect_to blogs_path
   end
   
-  private
+    private
 
-  def blog_params
+    def blog_params
       params.require(:blog).permit(:shout)
-  end
-  def set_blog
+    end
+    def set_blog
       @blog = Blog.find(params[:id])
-  end 
+    end 
 end
